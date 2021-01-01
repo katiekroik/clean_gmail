29424ff65d749391a3e26d6250344bdec69af54a
Jose Mendez <josermendez93@gmail.com>
2021-01-01 13:32:47 -0500

    Can now read, but not trash  emails

--- quickstart.py
+++ quickstart.py
@@ -18,7 +18,7 @@ from mimetypes import guess_type as guess_mime_type
 #Requesting permission to access/search for/delete emails
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://mail.google.com/']
-my_email = 'josermendez93@gmail.com'
+my_email = 'me'
 
 def search_messages(service, query):
     result = service.users().messages().list(userId='me', q=query).execute()
@@ -32,6 +32,154 @@ def search_messages(service, query):
         if 'messages' in result:
             messages.extend(result['messages'])
     return messages
+
+def mark_as_unread(service, query):
+    messages_to_mark = search_messages(service, query)
+    #add the label UNREAD to each of the search results
+
+    return service.users().messages().batchModify(
+        userId='me',
+        body={
+            'ids': [ msg['id'] for msg in messages_to_mark ],
+            'addLabelIds': ['UNREAD']
+        }
+    ).execute()
+
+# utility functions
+def get_size_format(b, factor=1024, suffix="B"):
+    """
+    Scale bytes to its proper byte format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if b < factor:
+            return f"{b:.2f}{unit}{suffix}"
+        b /= factor
+    return f"{b:.2f}Y{suffix}"
+
+def clean(text):
+    # clean text for creating a folder
+    return "".join(c if c.isalnum() else "_" for c in text)
+
+def parse_parts(service, parts, folder_name):
+    """
+    Utility function that parses the content of an email partition
+    """
+    if parts:
+        for part in parts:
+            filename = part.get("filename")
+            mimeType = part.get("mimeType")
+            body = part.get("body")
+            data = body.get("data")
+            file_size = body.get("size")
+            part_headers = part.get("headers")
+            if part.get("parts"):
+                # recursively call this function when we see that a part
+                # has parts inside
+                parse_parts(service, part.get("parts"), folder_name)
+            if mimeType == "text/plain":
+                # if the email part is text plain
+                if data:
+                    text = urlsafe_b64decode(data).decode()
+                    print(text)
+            elif mimeType == "text/html":
+                # if the email part is an HTML content
+                # save the HTML file and optionally open it in the browser
+                if not filename:
+                    filename = "index.html"
+                filepath = os.path.join(folder_name, filename)
+                print("Saving HTML to", filepath)
+                with open(filepath, "wb") as f:
+                    f.write(urlsafe_b64decode(data))
+            else:
+                # attachment other than a plain text or HTML
+                for part_header in part_headers:
+                    part_header_name = part_header.get("name")
+                    part_header_value = part_header.get("value")
+                    if part_header_name == "Content-Disposition":
+                        if "attachment" in part_header_value:
+                            # we get the attachment ID 
+                            # and make another request to get the attachment itself
+                            print("Saving the file:", filename, "size:", get_size_format(file_size))
+                            attachment_id = body.get("attachmentId")
+                            attachment = service.users().messages() \
+                                        .attachments().get(id=attachment_id, userId='me', messageId=msg['id']).execute()
+                            data = attachment.get("data")
+                            filepath = os.path.join(folder_name, filename)
+                            if data:
+                                with open(filepath, "wb") as f:
+                                    f.write(urlsafe_b64decode(data))                      
+
+def read_message(service, message_id):
+    """
+    This function takes Gmail API `service` and the given `message_id` and does the following:
+        - Downloads the content of the email
+        - Prints email basic information (To, From, Subject & Date) and plain/text parts
+        - Creates a folder for each email based on the subject
+        - Downloads text/html content (if available) and saves it under the folder created as index.html
+        - Downloads any file that is attached to the email and saves it in the folder created
+    """
+    msg = service.users().messages().get(userId='me', id=message_id['id'], format='full').execute()
+    # parts can be the message body, or attachments
+    payload = msg['payload']
+    headers = payload.get("headers")
+    parts = payload.get("parts")
+    folder_name = "email"
+    if headers:
+        # this section prints email basic info & creates a folder for the email
+        for header in headers:
+            name = header.get("name")
+            value = header.get("value")
+            if name == 'From':
+                # we print the From address
+                print("From:", value)
+            if name == "To":
+                # we print the To address
+                print("To:", value)
+            if name == "Subject":
+                # make a directory with the name of the subject
+                folder_name = clean(value)
+                # we will also handle emails with the same subject name
+                folder_counter = 0
+                while os.path.isdir(folder_name):
+                    folder_counter += 1
+                    # we have the same folder name, add a number next to it
+                    if folder_name[-1].isdigit() and folder_name[-2] == "_":
+                        folder_name = f"{folder_name[:-2]}_{folder_counter}"
+                    elif folder_name[-2:].isdigit() and folder_name[-3] == "_":
+                        folder_name = f"{folder_name[:-3]}_{folder_counter}"
+                    else:
+                        folder_name = f"{folder_name}_{folder_counter}"
+                os.mkdir(folder_name)
+                print("Subject:", value)
+            if name == "Date":
+                # we print the date when the message was sent
+                print("Date:", value)
+    parse_parts(service, parts, folder_name)
+    print("="*50)
+
+def delete_messages(service, query):
+    messages_to_delete = search_messages(service, query)
+    # it's possible to delete a single message with the delete API, like this:
+    # service.users().messages().delete(userId='me', id=msg['id'])
+    # but it's also possible to delete all the selected messages with one query, batchDelete
+    return service.users().messages().batchDelete(
+      userId='me',
+      body={
+          'ids': [ msg['id'] for msg in messages_to_delete]
+      }
+    ).execute()
+
+def trash_messages(service, message_id):
+    messages_to_trash = search_messages(service, query)
+    return service.users().messages.trash(
+      userId='me,
+      id=message_id
+      
+    ).execute()
+    
 
 def main():
     """Shows basic usage of the Gmail API.
@@ -60,17 +208,21 @@ def main():
     service = build('gmail', 'v1', credentials=creds)
 
     # Call the Gmail API
-    results = service.users().messages().list(userId='me').execute()
+    results = service.users().messages().list(userId=my_email).execute()
     messageIds = results.get('messages', [])
 
     if not messageIds:
         print('No message ids found.')
     else:
-        print(search_messages(my_email, 'google'))
-        '''print(results)
-        print('MessageIds:')
-        for id in messageIds:
-            print(id['id'])'''
+        query = 'Indeed'
+        messages = search_messages(service, query)
+        for message in messages:
+            #read_message(service, message)
+            #mark_as_unread(service, message)
+            trash_messages(service, message)
+            
+        #print(messages)
+        
             
 
 if __name__ == '__main__':
